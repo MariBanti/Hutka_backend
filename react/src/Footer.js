7fd8acccdede9fbe927a0a728c7bfe6f6a6261bb
asboff <egor622722@gmail.com>
2024-12-28 02:28:51 +0300

    final commit vol2

---
+++ react/src/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <div class="contacts">
+            <h1>Контакты</h1>
+            <a href="https://www.instagram.com/annakkkkra?igsh=eTJudGYwbzF6dDBt">
+                annakkkkra
+            </a>
+            - лучший бизнес-аналитик
+            <br></br>
+            <a href="https://www.instagram.com/_nabyram_/">_nabyram_</a>- лучший
+            фронтендер
+            <br></br>
+            <a href="https://www.instagram.com/k.polink?igsh=bzZsd2sxcjg3MWxm">
+                k.polink
+            </a>
+            - лучший дизайнер
+            <br></br>
+            <a href="https://www.instagram.com/videoblager_egor?igsh=MWhqNXU4bjk3cGU1dQ==">
+                videoblager_egor
+            </a>
+            - лучший бэкендер
+        </div>
+    );
+};
+export default Footer;
